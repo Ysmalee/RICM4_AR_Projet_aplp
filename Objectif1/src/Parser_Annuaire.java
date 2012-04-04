4de5d05dad80639a2991474a872444da34d7f68c
Timothée EID <timothee.eid@gmail.com>
2012-04-04 22:17:35 +0200

    Objectif 1 -> Fini => BIM

---
+++ Objectif1/src/Parser_Annuaire.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class Parser_Annuaire {
+	Document document;
+	
+	Parser_Annuaire(File fichier) throws IOException, ParserConfigurationException, SAXException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		//Using factory get an instance of document builder
+		DocumentBuilder db = dbf.newDocumentBuilder();
+
+		//parse using builder to get DOM representation of the XML file
+		document = db.parse(fichier.getPath());
+	}
+
+	Numero get_numero_from_xml(String nomCherche){
+		Element docEle = document.getDocumentElement();
+		
+		NodeList nl = docEle.getElementsByTagName("Telephone");
+		if(nl != null && nl.getLength() > 0) {
+			for(int i = 0; i<nl.getLength(); i++) {
+				Element el = (Element)nl.item(i);
+				if(el.getAttribute("name").equals(nomCherche)) {
+					return new Numero(el.getAttribute("numero"));
+				}
+				
+			}
+		}
+		return null;
+	}	
+}
